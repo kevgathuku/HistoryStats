526683f26f57f194903b317a466eae348caf95f5
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2024-09-17 11:58:20 +0200

    Improvements

--- Program.fs
+++ Program.fs
@@ -2,25 +2,27 @@
 open System.IO
 
 let parseHistoryLine (line: string) =
-    // Check if the line starts with ':', indicating the extended format
     if line.StartsWith(":") then
-        let command = line.Split([| ':'; ';' |], 4) |> Array.last
-        let bareCommand = command.Split([| ' ' |]) |> Array.head
-        bareCommand
+        let semiColonIndex = line.IndexOf(';')
+        // 1. Get the command i.e. everything after the semicolon
+        let fullCommand = line.Substring(semiColonIndex + 1)
+        // 2. Split by space to get the first part of the command
+        let command = fullCommand.Split([| ' ' |]) |> Array.head
+        Some command
     else
-        ""
+        None
 
 let commandsByFrequency count historyFile =
-    // Reads the lines in the file
+    // Returns an enumerable over the lines in the file
     File.ReadLines(historyFile)
-    // Filter out blank line
-    |> Seq.filter (fun line -> (String.length line) > 0)
+    // Take only the non-blank lines
+    |> Seq.choose (fun line -> if String.IsNullOrWhiteSpace line then None else Some line)
     // Extract the command from the line
-    |> Seq.map (fun line -> (parseHistoryLine line))
-    // Group by command
+    |> Seq.choose (fun line -> (parseHistoryLine line))
+    // Group by command -> (command, seq of commands)
     |> Seq.groupBy id
     // Count occurrences
-    |> Seq.map (fun (command, group) -> (command, Seq.length group))
+    |> Seq.map (fun (command, occurrences) -> (command, Seq.length occurrences))
     // Sort by the count in descending order
     |> Seq.sortByDescending snd
     // Take the top `count` elements
@@ -31,18 +33,14 @@ let commandsByFrequency count historyFile =
 let main argv =
     let args = Environment.GetCommandLineArgs()
 
-    // Print all the arguments
-    printfn "Command-line arguments: %A" args
-
-    // If there are specific arguments, print them
     match args with
-    | [| _app; firstArg |] ->
-        printfn "History file: %s" firstArg
-        let result = commandsByFrequency 5 firstArg
+    | [| _app; historyFile |] ->
+        printfn "History file: %s" historyFile
+        let result = commandsByFrequency 10 historyFile
 
-        for (cmd, count) in result do
+        for (cmd: string, count) in result do
             printfn "Command: %s \tCount: %d" cmd count
 
-    | _ -> printfn "Usage: dotnet run <firstArg>"
+    | _ -> printfn "Usage: dotnet run <historyFile>"
 
     0
